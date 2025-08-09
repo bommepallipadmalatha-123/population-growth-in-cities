995c220ec31f122f77cb8032d9154cd1e8757ed8
bommepallipadmalatha-123 <bommepallipadmalatha@gmail.com>
2025-08-09 18:03:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,76 +1,105 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
+
+# IMPORTING ALL THE LIBRARIES
+import pandas as pd 
 import numpy as np
+from sklearn.linear_model import LinearRegression
+import warnings
+warnings.filterwarnings("ignore")
+import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
+from numerize import numerize 
+st. set_page_config(layout="wide")
 
-# Population data: 2011 Census (official), 2024 & 2025 estimates from Technical Group / StatisticsTimes
-# (populations in individuals)
-data = {
-    'Year': [2011, 2024, 2025],
-    'Uttar Pradesh': [199812341, 236484000, 241300000],
-    'Bihar': [104099452, 129205000, 131000000],
-    'Maharashtra': [112374333, 126710000, None],
-    'West Bengal': [91276115, 99243000, None],
-    'Madhya Pradesh': [72626809, None, None],
-    'Tamil Nadu': [72147030, 76936000, 77165000],
-    'Rajasthan': [68548437, None, 83060000],
-    'Karnataka': [61095297, None, None],
-    'Gujarat': [60439692, None, 73513000],
-    'Andhra Pradesh': [84580777, None, None],
-    'Odisha': [41974218, None, None],
-    'Telangana': [None, 38317000, 38499000],
-    'Kerala': [33406061, None, 36111000],
-    'Jharkhand': [32988134, None, 40626000],
-    'Assam': [31205576, None, 36493000],
-    # ... additional states/UTs can be added similarly
-    'Delhi': [16787941, 21490000, 22277000],
-    'Punjab': [27743338, None, 31188000],
-    'Haryana': [25351462, None, 31057000],
-    'Chhattisgarh': [25545198, None, 30982000],
-    'Uttarakhand': [10086292, None, 11913000],
-    'Goa': [1458545, None, 1593000],
-    'Tripura': [3673917, None, 4232000],
-    'Meghalaya': [2966889, None, 3417000],
-    'Manipur': [2570390, None, 3289000],
-    'Nagaland': [1978502, None, 2258000],
-    'Mizoram': [1097206, None, 1264000],
-    'Sikkim': [610577, None, 703000],
-    'Andaman & Nicobar': [380581, None, 405000],
-    'Chandigarh': [1055450, None, 1257000],
-    'Puducherry': [1247953, None, 1732000],
-    'Lakshadweep': [64473, None, 69000],
-}
+# IMPORT DATASETS
+countries_pop = pd.read_csv(r'datasets/Countries_Population_final.csv')
+countries_name= pd.read_csv(r'datasets/Countries_names.csv')
 
-# Create DataFrame
-df = pd.DataFrame(data).set_index('Year')
+# DASHBOARD TITLE
+col1, col2,col3 = st.columns([2,6,2])
+with col1:
+    pass
+with col2:
+    st.info('# :blue[POPULATION PREDICTION SYSTEM]')
+with col3:
+    pass
+#mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm ML MODEL: POLYNOMIAL REGRESSION mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+col1, col2 = st.columns(2)
+with col1:
+    # USER INPUTS FOR PREDECTION
+    option = st.selectbox(
+        'PLEASE SELECT ANY COUNTRY',
+        (sorted(countries_name['Country_Name'])))
+    year = st.text_input('PLEASE ENTER YEAR', '2030')
+    if year.isnumeric():        
+        # Divide Independent and Dependent features
+        X=countries_pop['Year'] # all the independent features are copied to X
+        y=countries_pop[option] # the dependent feature is copied to y
 
-# Plot population trends
-plt.figure(figsize=(16, 12))
-for state in df.columns:
-    plt.plot(df.index, df[state], marker='o', label=state)
+        # Train Test splitting
+        from sklearn.model_selection import train_test_split
+        X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.30, random_state=42)
+        X_train = X_train.values.reshape(-1, 1)
+        X_test = X_test.values.reshape(-1, 1)
 
-plt.xlabel('Year')
-plt.ylabel('Population')
-plt.title('Population Trends of Indian States & UTs (2011, 2024, 2025)')
-plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
-plt.grid(True)
-plt.tight_layout()
-plt.show()
+        # SVR
+        from sklearn.preprocessing import PolynomialFeatures
+        from sklearn.metrics import r2_score
+        from sklearn.metrics import mean_squared_error
 
-# Forecasting function using linear regression
-def forecast_population(state_name, start_year=2025, end_year=2030):
-    y = df[state_name].dropna()
-    x = y.index.values.reshape(-1, 1)
-    model = LinearRegression().fit(x, y.values)
-    future_years = np.arange(start_year, end_year + 1).reshape(-1, 1)
-    preds = model.predict(future_years)
-    return list(zip(future_years.flatten(), preds.astype(int)))
+        def create_polynomial_regression_model(degree,Yearin):             
+            poly_features = PolynomialFeatures(degree=degree)          
+            X_train_poly = poly_features.fit_transform(X_train) # transforms the existing features to higher degree features.
+                 
+            poly_model = LinearRegression() 
+            poly_model.fit(X_train_poly, Y_train)    # fit the transformed features to Linear Regression    
+            y_train_predicted = poly_model.predict(X_train_poly) # predicting on training data-set
+            y_test_predict = poly_model.predict(poly_features.fit_transform(X_test)) # predicting on test data-set
+            output = poly_model.predict(poly_features.fit_transform([[Yearin]]))           
+        
+            # rmse_train = np.sqrt(mean_squared_error(Y_train, y_train_predicted)) # evaluating the model on training dataset
+            # r2_train = r2_score(Y_train, y_train_predicted)        
+            # rmse_test = np.sqrt(mean_squared_error(Y_test, y_test_predict))  # evaluating the model on test dataset            
+            r2_test = r2_score(Y_test, y_test_predict)
+        
+            # #st.write("RMSE of training set is {}".format(rmse_train))
+            # st.write("TRAINING SET ACCURACY {}".format(r2_train))
+            # st.write("-------------------------------------------")
+            # #st.write("RMSE of test set is {}".format(rmse_test))
+            # st.write("TEST SET ACCURACY {}".format(r2_test))
+            # st.write("-------------------------------------------")
+            st.write("#### :green[ALGORITHM: ] POLYNOMIAL REGRESSION")
+            st.write('#### :green[ ACCURACY: ] '+str(int(r2_test*100))+'%')  
+            return output
 
-# Example: Forecasting for Uttar Pradesh
-state_to_forecast = 'Uttar Pradesh'
-forecast = forecast_population(state_to_forecast)
+        pred= create_polynomial_regression_model(2,int(year))
 
-print(f"\nForecasted population for {state_to_forecast} (2026-2030):")
-for year, pop in forecast:
-    print(f"{year}: {pop:,}")
+        # OUTPUT DETAILS        
+        pred_pop = numerize.numerize(pred[0])
+        st.write("#### :green[COUNTRY:  ] "+option.upper())
+        st.write("#### :green[YEAR:  ] "+year)
+        st.write("#### :green[PREDICTED POPULATION:  ] "+pred_pop)
+    else:
+        st.write('PLEASE ENTER A VALID YEAR')
+
+with col2:
+    if year.isnumeric():
+        st.write('#### :green['+option.upper()+"'S  POPULATION]")
+        fig1 = go.Figure()
+        # Create and style traces
+        fig1.add_trace(go.Scatter(x=countries_pop['Year'] , y=countries_pop[option], name = "Previous Year's",
+                                line=dict(color='green', width=11)
+                                ))
+        fig1.add_trace(go.Scatter(x=[year], y=[pred[0]], 
+                                name = 'Predicted '+year,
+                                mode = 'markers',
+                                marker_symbol = 'star',
+                                marker=dict(
+                                size=20, 
+                                color=np.random.randn(500), #set color equal to a variable
+                                colorscale='reds', # one of plotly colorscales                                
+                                )
+                                ))
+        st.plotly_chart(fig1)
+        st.write('The above plot shows the population of a country from 1960 to 2021, star represents the predicted population for the given year')
 
